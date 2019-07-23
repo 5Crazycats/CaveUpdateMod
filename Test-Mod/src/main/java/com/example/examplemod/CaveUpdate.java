f5146971b6d0f24221fd97844f00cf75237c73f3
5Crazycats <mailforhomer@gmail.com>
2019-07-23 13:37:40 -0400

    adding all files

---
+++ Test-Mod/src/main/java/com/example/examplemod/CaveUpdate.java
@@ -0,0 +1,29 @@
+package minecraftfun;
+ 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+ 
+public class TargetBlock extends Block {
+    public static final String NAME = "target";
+    private static final String UNLOCALIZED_NAME = "cave_update";
+    private static final String REGISTRY_NAME = "cave_update_registry";
+ 
+    public TargetBlock() {
+        super(Material.ROCK);
+ 
+        this.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+ 
+        this.setUnlocalizedName(UNLOCALIZED_NAME);
+        this.setRegistryName(REGISTRY_NAME);
+    }
+ 
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+      return BlockRenderLayer.SOLID;
+    }
+}
